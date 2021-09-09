bff1b589684154b65dc35d8f9081f827b962e623
Renzo <renzo.portilla@tecsup.edu.pe>
2021-09-08 23:11:21 -0500

    cambio2

--- src/main/java/com/tecsup/lab3/model/Student.java
+++ src/main/java/com/tecsup/lab3/model/Student.java
@@ -5,6 +5,6 @@ public class Student {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String estudiante="Renzo Portilla";
+		String estudiante="Hola Mundo";
 	}
 }
