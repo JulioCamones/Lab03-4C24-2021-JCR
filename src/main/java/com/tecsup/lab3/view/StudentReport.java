1ec7c42b6a7658c14e40ca26b12cc065f8b14f7a
user <user@DESKTOP-4CT8K85>
2021-09-08 22:10:35 -0500

    Como estas

--- src/main/java/com/tecsup/lab3/view/StudentReport.java
+++ src/main/java/com/tecsup/lab3/view/StudentReport.java
@@ -4,7 +4,7 @@ public class StudentReport {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String estudiante="Renzo Portilla";
+		String estudiante="Julio Camones";
 	}
 
 }
