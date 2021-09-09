e369eec57cfa823a5bbf2ff9959e4b83ad487918
user <user@DESKTOP-4CT8K85>
2021-09-08 22:08:57 -0500

    Hola

--- src/main/java/com/tecsup/lab3/view/StudentReport.java
+++ src/main/java/com/tecsup/lab3/view/StudentReport.java
@@ -1,5 +1,10 @@
 package com.tecsup.lab3.view;
 
 public class StudentReport {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String estudiante="Renzo Portilla";
+	}
 
 }
